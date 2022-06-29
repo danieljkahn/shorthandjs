9c4effbf2e58187bec248fd3bbad66c845145418
Daniel Kahn <danieljkahn@gmail.com>
2022-06-28 20:45:33 -0400

    today's edits

--- shorthand.js
+++ shorthand.js
@@ -1,4 +1,19 @@
-var $ = document.querySelectorAll.bind(document);
+var _ = document.querySelectorAll.bind(document);
+
+// window / doc width
+var width = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+
+// window / doc height
+
+var height = window.innerHeight
+|| document.documentElement.clientHeight
+|| document.body.clientHeight;
+
+// on doc ready
+
+// on resize
 
 /// Utility functions to add/remove css classes and mimick jquery's hide/show functions
 
@@ -14,3 +29,11 @@ NodeList.prototype.removeClass = function(className) {
     });
     return this;
 }
+NodeList.prototype.click = function(fn) {
+    this.forEach(n => {
+        n.addEventListener('click', fn);
+    })
+    return this;
+}
+
+
